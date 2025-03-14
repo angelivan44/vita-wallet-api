44d011ef66781b908014d0297951e0f7c7411f2f
Angel <angelhuays@gmail.com>
2025-03-14 09:24:37 -0500

    Remove asset precompilation and cleaning commands from render-build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,7 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+
 
 # Como estás usando una instancia gratuita, necesitas
 # realizar las migraciones de la base de datos en el comando de construcción
